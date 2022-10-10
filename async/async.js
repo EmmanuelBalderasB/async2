3e3e14c1e5021e9550125ec1a0eb7b0ccbd47b1f
EmmanuelBalderasB <ebalderasbeq@gmail.com>
2022-10-10 04:39:56 -0500

    Removes whitespace

--- async/async.js
+++ async/async.js
@@ -1,6 +1,5 @@
 const token = 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;
 const songs = require('../modules/main.js')
 async function chain() {
-
     let onResolve = await songs.getSong('upbeat')
 }
